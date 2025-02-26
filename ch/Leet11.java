790a7540ae858fb62659aa54d8b54a622d4d4f1d
chhong <pnci1029@gmail.com>
2025-02-26 22:41:40 +0900

    02_26

---
+++ ch/Leet11.java
@@ -0,0 +1,26 @@
+package ch;
+
+class Solution {
+    public int maxArea(int[] height) {
+
+        int first = 0;
+        int last = height.length -1;
+        int result = 0;
+        while(true){
+            if(first>last) break;
+
+            int width = last - first;
+            int heights = (Math.min(height[last], height[first]));
+            result = Math.max(result, width * heights);
+
+            if(height[first] > height[last]){
+                last--;
+            }else{
+                first++;
+            }
+        }
+
+        return result;
+    }
+}
+
