09ddd9d077d86e166fe73299fa2ad0ec173b7f1e
chhong <pnci1029@gmail.com>
2025-02-28 13:34:56 +0900

    02_26

---
+++ ch/Leet643.java
@@ -0,0 +1,19 @@
+package ch;
+
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+
+        double currentValue = 0;
+        for(int i =0; i<k; i++){
+            currentValue += nums[i];
+        }
+        double result = currentValue;
+
+        for(int i =k; i<nums.length; i++){
+            currentValue = currentValue - nums[i-k] + nums[i];
+            result = Math.max(result, currentValue);
+        }
+
+        return result/k;
+    }
+}
