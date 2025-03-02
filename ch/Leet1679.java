b6aae14b771f01a7f03823472d2a121405fdae09
chhong <pnci1029@gmail.com>
2025-03-02 18:56:33 +0900

    03_02

---
+++ ch/Leet1679.java
@@ -0,0 +1,33 @@
+package ch;
+
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+
+        if(nums.length ==0){
+            return 0;
+        }
+        Arrays.sort(nums);
+
+        int first = 0;
+        int last = nums.length - 1;
+        int count = 0;
+
+        while(first < last){
+            int sum = nums[first] + nums[last];
+            if(sum == k){
+                first++;
+                last--;
+                count++;
+            }else {
+                if(sum >k){
+                    last--;
+                }else{
+                    first++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
+
