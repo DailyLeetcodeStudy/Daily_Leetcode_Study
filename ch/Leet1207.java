ad12ab2a9018332506d61497032570317a4b8854
chhong <pnci1029@gmail.com>
2025-03-10 19:25:14 +0900

    03_10

---
+++ ch/Leet1207.java
@@ -0,0 +1,18 @@
+package ch;
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+
+        for(int value: arr){
+            map.put(value, map.getOrDefault(value, 0) + 1);
+        }
+
+        for (int value : map.keySet()) {
+            set.add(map.get(value));
+        }
+
+        return map.size() == set.size();
+    }
+}
