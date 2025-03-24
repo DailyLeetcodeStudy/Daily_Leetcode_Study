cbe4a6dcb1af8b0c164b92d1f68588c30d9e631d
chhong <pnci1029@gmail.com>
2025-03-24 19:18:22 +0900

    03_24

---
+++ ch/Leet2390.java
@@ -0,0 +1,24 @@
+package ch;
+
+class Solution {
+    public String removeStars(String s) {
+        Stack<String> stack = new Stack<>();
+        String [] split = s.split("");
+        String result = "";
+
+        for(String value : split){
+            if(value.equals("*")){
+                result += stack.pop();
+            }else {
+                stack.add(value);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(String value: stack){
+            sb.append(value);
+        }
+
+        return sb.toString();
+    }
+}
